e5ca165ed2ffd8ba0a8f8bea9a62f67fe9c7ff8c
Ferihan HACIALİOĞLU <feri.han34@hotmail.com>
2024-03-24 17:56:10 +0300

    doktor profil ve ilgili doktora ait rezervasyon listes

---
+++ khasGraduationProject/Controllers/DoctorAppointmentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace khasGraduationProject.Controllers
+{
+    public class DoctorAppointmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
