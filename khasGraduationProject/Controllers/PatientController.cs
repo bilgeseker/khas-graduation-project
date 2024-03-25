e35089d308d0aa282f73115aa251c56130cb33dc
bilgeseker <bilgeseker751@gmail.com>
2024-03-25 21:00:16 +0300

    patient login added

---
+++ khasGraduationProject/Controllers/PatientController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace khasGraduationProject.Controllers
+{
+    
+    public class PatientController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
+
