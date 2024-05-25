5f1ae76bc41b90c3ee52ef964c0a5bb0d1bf89a6
Ferihan HACIALİOĞLU <feri.han34@hotmail.com>
2024-05-25 20:49:50 +0300

    ContactUs sayfası eklendi

--- khasGraduationProject/Controllers/HomeController.cs
+++ khasGraduationProject/Controllers/HomeController.cs
@@ -25,6 +25,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult ContactUs()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
